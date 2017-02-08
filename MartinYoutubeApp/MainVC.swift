e56b5117837e8a6573e13eb5616cb4b3bf485833
Martin Nordström <martinnord@martin-nord.guest.stockholm.se>
2017-02-08 18:13:24 +0100

    Fixed the UI

---
+++ MartinYoutubeApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MartinYoutubeApp
+//
+//  Created by Martin Nordström on 2017-02-08.
+//  Copyright © 2017 Martin Nordström. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
